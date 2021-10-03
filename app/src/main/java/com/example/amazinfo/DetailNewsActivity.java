fd8202748d0dc56ac3a4968462cedb450d2dcfbc
White Pegasus <whitepegasus1603@gmail.com>
2021-10-03 18:01:04 +0530

    Second Commit

--- app/src/main/java/com/example/amazinfo/DetailNewsActivity.java
+++ app/src/main/java/com/example/amazinfo/DetailNewsActivity.java
@@ -2,7 +2,9 @@ package com.example.amazinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 
 import com.bumptech.glide.Glide;
 import com.example.amazinfo.Modals.ArticleModal;
@@ -22,10 +24,13 @@ public class DetailNewsActivity extends AppCompatActivity {
         article=(ArticleModal) getIntent().getSerializableExtra("data");
 
         binding.newsHeadline.setText(article.getTitle());
-        binding.newsAuthor.setText(article.getAuthor());
+        binding.newsAuthor.setText("Source: "+article.getAuthor());
         binding.newsTime.setText(article.getPublishedAt());
         binding.newsDescription.setText(article.getDescription());
         binding.newsContent.setText(article.getContent());
         Glide.with(this).load(article.getUrlToImage()).into(binding.newsImage);
+
+        binding.instaLink.setMovementMethod(LinkMovementMethod.getInstance());
+        binding.instaLink.setLinkTextColor(Color.YELLOW);
     }
 }
--- app/src/main/java/com/example/amazinfo/MainActivity.java
+++ app/src/main/java/com/example/amazinfo/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.amazinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.amazinfo.Adapters.customAdapter;
 import com.example.amazinfo.Interfaces.OnArticleClickedListener;
@@ -16,11 +20,12 @@ import com.example.amazinfo.databinding.ActivityMainBinding;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnArticleClickedListener{
+public class MainActivity extends AppCompatActivity implements OnArticleClickedListener, View.OnClickListener{
     ActivityMainBinding binding;
     //RecyclerView recyclerView;
     customAdapter adapter;
     ProgressDialog dialog;
+    String selectedCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +34,58 @@ public class MainActivity extends AppCompatActivity implements OnArticleClickedL
         setContentView(binding.getRoot());
 
         dialog=new ProgressDialog(this);
-        dialog.setTitle("Fetching new headlines...");
+        dialog.setTitle("Fetching news headlines...");
         dialog.show();
+        selectedCategory="sports";
         apiRequestManager manager=new apiRequestManager(this);
         manager.getNews(listener,"sports",null);
+
+
+        binding.businessBtn.setOnClickListener(this);
+        binding.technologyBtn.setOnClickListener(this);
+        binding.scienceBtn.setOnClickListener(this);
+        binding.generalBtn.setOnClickListener(this);
+        binding.healthBtn.setOnClickListener(this);
+        binding.sportsBtn.setOnClickListener(this);
+        binding.entertainmentBtn.setOnClickListener(this);
+
+        binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                dialog.setTitle("Fetching news articles for "+query);
+                dialog.setCancelable(false);
+                dialog.show();
+                apiRequestManager manager=new apiRequestManager(MainActivity.this);
+                manager.getNews(listener,selectedCategory,query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
     }
 
     private final OnFetchDataCustomListener<NewsApiResponse> listener= new OnFetchDataCustomListener<NewsApiResponse>() {
         @Override
         public void onFetchData(List<ArticleModal> list, String message) {
-            showNews(list);
-            dialog.dismiss();
+            if(list.isEmpty())
+            {
+                Toast.makeText(MainActivity.this, "No news article found!", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+            else {
+                showNews(list);
+                dialog.dismiss();
+            }
+
         }
 
         @Override
         public void onError(String message) {
-
+            Toast.makeText(MainActivity.this, "Some error occured!", Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -58,5 +99,17 @@ public class MainActivity extends AppCompatActivity implements OnArticleClickedL
     @Override
     public void OnArticleClicked(ArticleModal headlines) {
         startActivity(new Intent(this,DetailNewsActivity.class).putExtra("data",headlines));
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button btn= (Button) view;
+        String category=btn.getText().toString();
+        selectedCategory=category;
+        dialog.setTitle("Fetching news of "+category+" category");
+        dialog.setCancelable(false);
+        dialog.show();
+        apiRequestManager manager=new apiRequestManager(this);
+        manager.getNews(listener,category,null);
     }
 }
